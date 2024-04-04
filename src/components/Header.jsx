d0d10277a97d2bfab18077ef567e32719cf7e5f1
vikramsingh2110 <singhvikram102000@gmail.com>
2024-04-04 12:04:40 +0530

    Done with project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,8 +50,8 @@ const Header = () => {
     });
   }
   return (
-   <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex p-2 justify-between'>
-      <img className='w-44' src= {LOGO}
+   <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row p-2 justify-between '>
+      <img className='w-44 mx-auto md:mx-0' src= {LOGO}
       alt='logo' />
     
      {user && (
